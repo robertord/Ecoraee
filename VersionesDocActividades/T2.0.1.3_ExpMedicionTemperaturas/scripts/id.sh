5ecbcb6e68664687b6e3aabfe8939a463e3fe8fb
Roberto Rosende Dopazo <roberto.rosende.dopazo@gmail.com>
2014-01-15 14:03:35 +0100

    Limpiados datos de ips

--- VersionesDocActividades/T2.0.1.3_ExpMedicionTemperaturas/scripts/id.sh
+++ VersionesDocActividades/T2.0.1.3_ExpMedicionTemperaturas/scripts/id.sh
@@ -2,4 +2,4 @@
 FICH=`date +%Y.%m.%d.%H.%M`_`hostname`
 echo "Hola soy "`hostname`" y tengo la ip: " > /tmp/$FICH
 ifconfig eth| grep -Eo "inet addr:[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+"|grep -Eo "[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+">>/tmp/$FICH
-scp /tmp/$FICH cluster@192.168.40.102:/home/cluster/arranque/
+scp /tmp/$FICH cluster@xxx.xxx.xxx.xxx:/home/cluster/arranque/
